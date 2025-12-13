4bc8b7ae32c90fe31b22e97be2b93299035f8a0c
MilanPavasiya <milpavasiya@gmail.com>
2025-12-13 06:47:30 -0500

    multer and cloudinary configuration setup

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,2 +1,20 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+cloudinary.v2.uploader
+  .upload(
+    "my_image.jpg",
+    { public_id: "sample_image" },
+    function (error, result) {
+      if (error) {
+        console.error("Error uploading to Cloudinary:", error);
+      }
+    }
+  )
+  .then((result) => console.log(result));
