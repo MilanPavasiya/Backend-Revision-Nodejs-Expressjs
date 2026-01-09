4a7b5112704654c3bf3a3b2b622d209144b8b54b
MilanPavasiya <milpavasiya@gmail.com>
2026-01-09 14:28:07 -0500

    fix: correct syntax for owner field in video schema

--- backend/src/models/video.model.js
+++ backend/src/models/video.model.js
@@ -32,9 +32,9 @@ const videoSchema = new Schema(
       default: true,
     },
     owner: {
-        type: Schema.Types.ObjectId;
-        ref: "User"
-    }
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   { timestamps: true }
 );
